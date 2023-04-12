cd643efbdf3af222af05c8b8a5ffaa32f42f6bcb
Shaon Majumder <smazoomder@gmail.com>
2023-04-13 03:51:18 +0600

    Projects

--- sprint-management/src/main/java/SpringBootRestApplication.java
+++ sprint-management/src/main/java/SpringBootRestApplication.java
@@ -8,3 +8,25 @@
 //        SpringApplication.run(SpringBootRestApplication.class, args);
 //    }
 //}
+
+import org.springframework.context.annotation.ComponentScan;
+
+//@ComponentScan(basePackages = {""})
+
+import BACKEND.Routes.RouteConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+@Import(RouteConfiguration.class)
+@EnableAutoConfiguration(exclude = {R2dbcAutoConfiguration.class})
+@SpringBootApplication
+public class SpringBootRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApplication.class, args);
+    }
+
+}
